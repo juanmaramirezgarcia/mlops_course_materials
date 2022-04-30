20dbe429cafaa6a90b75ba357a647e67e51703e1
Juan Ramirez <juanma.ramirezgarcia@gmail.com>
2022-04-30 19:02:31 +0200

    first commit s03 and s04

---
+++ 04 MLFlow/Wine/score.py
@@ -0,0 +1,24 @@
+# Example of using mlflow logged model to score 
+
+import mlflow
+logged_model = 'runs:/6eb6df92b60e422c8fbc6b89f4425a66/model'
+
+# Load model as a PyFuncModel.
+loaded_model = mlflow.pyfunc.load_model(logged_model)
+
+# Predict on a Pandas DataFrame.
+import pandas as pd
+
+model1 = mlflow.sklearn.load_model(logged_model)
+
+data = pd.read_csv('winequality-red.csv', sep=";")
+
+# Selecting a random row from the data to score
+score_x = data.drop(["quality"], axis=1).sample()
+
+from tabulate import tabulate
+print('vector ->')
+print(tabulate(score_x, headers='keys', tablefmt='psql'))    
+
+res = loaded_model.predict(pd.DataFrame(score_x))
+print('score -> ', res[0])
---
+++ README.md
@@ -0,0 +1,3 @@
+# MLOps Course Repository
+
+I will be adding here all the Python examples that I used in class
